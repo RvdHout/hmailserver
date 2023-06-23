953cbc8e43dd7b8f34d086474f2869251cd19cd8
RvdHout <ruud.vd.hout@hotmail.com>
2023-06-23 08:43:33 +0200

    INI Setting
    Add "AddReceivedSPFHeader" INI setting

--- hmailserver/source/Server/Common/Application/IniFileSettings.cpp
+++ hmailserver/source/Server/Common/Application/IniFileSettings.cpp
@@ -60,7 +60,8 @@ namespace HM
       smtpdmax_size_drop_(0),
       backup_messages_dbonly_(false),
       add_xauth_user_ip_(false),
-      use_dns_cache_(true)
+      use_dns_cache_(true),
+      add_received_spf_header_(false)
 
    {
 
@@ -194,10 +195,11 @@ namespace HM
       blocked_iphold_seconds_ =  ReadIniSettingInteger_("Settings", "BlockedIPHoldSeconds", 0);
       smtpdmax_size_drop_ =  ReadIniSettingInteger_("Settings", "SMTPDMaxSizeDrop", 0);
       backup_messages_dbonly_ =  ReadIniSettingInteger_("Settings", "BackupMessagesDBOnly", 0) == 1;
-      add_xauth_user_ip_ =  ReadIniSettingInteger_("Settings", "AddXAuthUserIP", 1) == 1;
+      add_xauth_user_ip_ =  ReadIniSettingInteger_("Settings", "AddXAuthUserIP", 0) == 1;
       use_dns_cache_ = ReadIniSettingInteger_("Settings", "UseDNSCache", 1) == 1;
       dns_server_ = ReadIniSettingString_("Settings", "DNSServer", "");
       rewrite_envelope_from_when_forwarding_ = ReadIniSettingInteger_("Settings", "RewriteEnvelopeFromWhenForwarding", 0) == 1;
+      add_received_spf_header_ = ReadIniSettingInteger_("Settings", "AddReceivedSPFHeader", 0) == 1;
       m_sDisableAUTHList = ReadIniSettingString_("Settings", "DisableAUTHList", "");
    }
 
