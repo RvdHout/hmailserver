6f37676bfd8677746aced40e4d44cd4f341516d3
RvdHout <ruud.vd.hout@hotmail.com>
2024-12-27 14:55:48 +0100

    DBScripts
    Fix upgrade scripts for MSSQL & MSSQLCE

--- hmailserver/source/DBScripts/Upgrade5601to5606MSSQL.sql
+++ hmailserver/source/DBScripts/Upgrade5601to5606MSSQL.sql
@@ -6,6 +6,6 @@ insert into hm_settings (settingname, settingstring, settinginteger) values ('as
 
 insert into hm_settings (settingname, settingstring, settinginteger) values ('IPv6Preferred', '', 0)
 
-alter table hm_fetchaccounts add column famimerecipientheaders nvarchar(255) not null default 'To,CC,X-RCPT-TO,X-Envelope-To'
+alter table hm_fetchaccounts add famimerecipientheaders nvarchar(255) not null default 'To,CC,X-RCPT-TO,X-Envelope-To'
 
 update hm_dbversion set value = 5606
