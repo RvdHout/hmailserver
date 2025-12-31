22189fc4fe346713f7cd350eba374dd94d848303
RvdH <ruud.vd.hout@hotmail.com>
2025-12-31 10:06:37 +0100

    DBScripts
    PostgreSQL DB Upgrade error reported thru forum pm, do same for MySQL upgrade scripts, MSSQL already had default value

--- hmailserver/source/DBScripts/Upgrade5606to5609MySQL.sql
+++ hmailserver/source/DBScripts/Upgrade5606to5609MySQL.sql
@@ -1,8 +1,8 @@
-alter table hm_accounts add column accountvacationabortspamflagged tinyint not null;
+alter table hm_accounts add column accountvacationabortspamflagged tinyint not null default 0;
 
-alter table hm_accounts add column accountforwardabortspamflagged tinyint not null;
+alter table hm_accounts add column accountforwardabortspamflagged tinyint not null default 0;
 
-alter table hm_rule_actions add column actionabortspamflagged tinyint not null;
+alter table hm_rule_actions add column actionabortspamflagged tinyint not null default 0;
 
 alter table hm_messages modify column messageflags tinyint unsigned not null;
 
